424ba872dcf0ec4a72dd80ca16044403f4943749
manoj satwase <manojsatwase123>
2020-02-04 20:49:38 +0530

    todo-list

---
+++ src/components/TodoInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class TodoInput extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hello from todo item input</h1>
+      </div>
+    );
+  }
+}
---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class TodoItem extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Item from todo</h1>
+      </div>
+    );
+  }
+}
